114a5273882767a05df16a15707e441fdb685bb6
David <91611885+FEDavid@users.noreply.github.com>
2024-07-03 15:26:10 +0100

    Updated grid on postComponent as the height was not matching should other elements in row be larger

--- src/components/postComponent.js
+++ src/components/postComponent.js
@@ -4,23 +4,21 @@ import '../styles.css';
 
 function Component(props) {
   return (
-    <div className='grid'>
-      <div className="w-full justify-self-center md:w-1/2 xl:w-full">
-        <div className='h-64 overflow-hidden'>
-          <a href={props.link} target='blank'>
-            <img src={props.thumbnail} alt="My logo" className='object-cover w-full h-full transition hover:opacity-75' />
-          </a>
-        </div>
-        <div className='p-6 text-left text-white custom_gradient md:rounded-b-2xl'>
-          <h2 className='text-2xl font-semibold'>{props.title}</h2>
-          <p className='mt-4 opacity-80 h-[100px] text-ellipsis overflow-hidden line-clamp-4 transition-opacity'>{props.content}</p>
-          <div className='mt-6 flex text-sm text-[--custom_blue] gap-4 flex-wrap'>
-            {props.stack.map((item, index) => (
-              <p key={index} className='px-3 pb-0.5 bg-[--custom_lime] rounded-3xl hover:opacity-80 transition-opacity'>
-                {item}
-              </p>
-            ))}
-          </div>
+    <div className="w-full justify-self-center md:w-1/2 xl:w-full grid grid-rows-[min-content_1fr]">
+      <div className='h-64 overflow-hidden'>
+        <a href={props.link} target='blank'>
+          <img src={props.thumbnail} alt="My logo" className='object-cover w-full h-full transition hover:opacity-75' />
+        </a>
+      </div>
+      <div className='p-6 text-left text-white custom_gradient md:rounded-b-2xl'>
+        <h2 className='text-2xl font-semibold'>{props.title}</h2>
+        <p className='mt-4 opacity-80 h-[100px] text-ellipsis overflow-hidden line-clamp-4 transition-opacity'>{props.content}</p>
+        <div className='mt-6 flex text-sm text-[--custom_blue] gap-4 flex-wrap'>
+          {props.stack.map((item, index) => (
+            <p key={index} className='px-3 pb-0.5 bg-[--custom_lime] rounded-3xl hover:opacity-80 transition-opacity'>
+              {item}
+            </p>
+          ))}
         </div>
       </div>
     </div>
