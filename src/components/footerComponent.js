64c9561a77e4b4e7b89aab33164612a389f14643
David <91611885+FEDavid@users.noreply.github.com>
2024-07-26 19:41:53 +0100

    Minor UI changes due to spacing on footer

--- src/components/footerComponent.js
+++ src/components/footerComponent.js
@@ -11,19 +11,19 @@ function Component() {
   const lt_component = <SocialIcon bgColor="#D4DE29" fgColor="#021117" url="https://linktr.ee/dmould" />
 
   return (
-    <div className="bg-[--custom_lime] h-min self-end text-xl font-bold text-[--custom_blue] p-3 flex justify-between whitespace-nowrap ">
-      <h2 className="font-[Trebuchet_MS] text-xl w-min whitespace-nowrap mr-5 select-none self-center">david-mould.<span className='font-black'>DEV</span></h2>
+    <div className="bg-[--custom_lime] h-min self-end text-xl font-bold text-[--custom_blue] p-2 flex justify-between whitespace-nowrap ">
+      <h2 className="font-[Trebuchet_MS] text-xl w-min whitespace-nowrap mr-4 select-none self-center">david-mould.<span className='font-black'>DEV</span></h2>
       {/* <ul id="socials" class="text-sm">
         <li class="inline-block">{gh_component}</li>
         <li class="inline-block mx-2">{li_component}</li>
         <li class="inline-block">{lt_component}</li>
       </ul> */}
-      <Link to="/sitemap" className='hover:opacity-50 transition-opacity'>
-        <div className='flex gap-1 dm-sans-regular-font text-base'>
-          <span class="material-symbols-outlined self-center">map</span>
-          <p className='relative'>Sitemap</p>
-        </div>
+      
+      <Link to="/sitemap" className='hover:opacity-50 transition-opacity flex gap-1 dm-sans-regular-font text-base'>
+        <span class="material-symbols-outlined self-center">map</span>
+        <p className='relative'>Sitemap</p>
       </Link>
+      
     </div>
   );
 }
